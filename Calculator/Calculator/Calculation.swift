c99e21d2a0e814043335a64e728b96c59bee70b4
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-05-29 18:21:52 +0900

    feature: Calculator class for calculating values. working object by singleton

---
+++ Calculator/Calculator/Calculation.swift
@@ -0,0 +1,61 @@
+//
+//  Calculation.swift
+//  Calculator
+//
+//  Created by yudonlee on 2022/05/29.
+//
+
+import Foundation
+
+class Calculation {
+//    SingleTon으로 계산기에 저장된 값들을 관리한다.
+    static let calculator: Calculation = Calculation()
+    
+    var numberArray: [Double] = []
+    var operatorArray: [String] = []
+    
+    func pushNumber(_ element: String?) {
+        if let element = element, let strToDouble = Double(element){
+            numberArray.append(strToDouble)
+        }
+    }
+    
+    func pushOperator(_ element: String?) -> String?{
+        if let element = element {
+            operatorArray.append(element)
+        }
+        
+        if operatorArray.count == 2 {
+            let calculatedResult = valuesCalculation()
+            if calculatedResult == Double(Int(calculatedResult)) {
+                return String(Int(calculatedResult))
+            }
+            return String(calculatedResult)
+        }
+        return nil
+    }
+    
+    func allClear() {
+        numberArray = []
+        operatorArray = []
+    }
+    
+    func valuesCalculation() -> Double{
+        var result: Double
+        switch operatorArray.removeFirst() {
+        case "+":
+            result = numberArray[0] + numberArray[1]
+        case "-":
+            result = numberArray[0] - numberArray[1]
+        case "/":
+            result = numberArray[0] / numberArray[1]
+        case "*":
+            result = numberArray[0] * numberArray[1]
+        
+        default:
+            result = 0
+        }
+        numberArray = [result]
+        return result
+    }
+}
