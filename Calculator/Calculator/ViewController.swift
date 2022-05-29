b4c290b588900c5aa4c4cb34fb7d7671028729b8
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-05-29 11:48:37 +0900

    feature calculator button by adding IBOutlet Button

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textLabel: UILabel!
+    
+    @IBOutlet weak var oneButton: UIButton!
+    @IBOutlet weak var twoButton: UIButton!
+    @IBOutlet weak var threeButton: UIButton!
+    @IBOutlet weak var fourButton: UIButton!
+    @IBOutlet weak var fiveButton: UIButton!
+    @IBOutlet weak var sixButton: UIButton!
+    @IBOutlet weak var sevenButton: UIButton!
+    @IBOutlet weak var eightButton: UIButton!
+    
+    @IBOutlet weak var nineButton: UIButton!
+    @IBOutlet weak var zeroButton: UIButton!
+    
+    @IBOutlet weak var dotButton: UIButton!
+    @IBOutlet weak var equalButton: UIButton!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var multiButton: UIButton!
+    @IBOutlet weak var divideButton: UIButton!
+    
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var signToggleButton: UIButton!
+    @IBOutlet weak var percentButton: UIButton!
+    
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+}
+extension ViewController {
 
 }
 
