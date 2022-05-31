700ec35fb3b69cc184bfcfc29e930aeb29d10d15
yudonlee <39371835+yudonlee@users.noreply.github.com>
2022-05-31 13:57:22 +0900

    Apply SwiftLint

--- Calculator/Calculator/Calculation.swift
+++ Calculator/Calculator/Calculation.swift
@@ -10,21 +10,19 @@ import Foundation
 class Calculation {
 //    SingleTon으로 계산기에 저장된 값들을 관리한다.
     static let calculator: Calculation = Calculation()
-    
+    private init() { }
     var numberArray: [Double] = []
     var operatorArray: [String] = []
-    
     func pushNumber(_ element: String?) {
-        if let element = element, let strToDouble = Double(element){
+        if let element = element,
+            let strToDouble = Double(element) {
             numberArray.append(strToDouble)
         }
     }
-    
-    func pushOperator(_ element: String?) -> String?{
+    func pushOperator(_ element: String?) -> String? {
         if let element = element {
             operatorArray.append(element)
         }
-        
         if operatorArray.count == 2 {
             let calculatedResult = valuesCalculation()
             if calculatedResult == Double(Int(calculatedResult)) {
@@ -34,13 +32,11 @@ class Calculation {
         }
         return nil
     }
-    
     func allClear() {
         numberArray = []
         operatorArray = []
     }
-    
-    func valuesCalculation() -> Double{
+    func valuesCalculation() -> Double {
         var result: Double
         switch operatorArray.removeFirst() {
         case "+":
@@ -51,7 +47,6 @@ class Calculation {
             result = numberArray[0] / numberArray[1]
         case "*":
             result = numberArray[0] * numberArray[1]
-        
         default:
             result = 0
         }
--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,11 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var textLabel: UILabel!
-    
     private var inputString: String?
     private var isOperationNow: Bool = false
-    
-    
     @IBOutlet weak var oneButton: UIButton!
     @IBOutlet weak var twoButton: UIButton!
     @IBOutlet weak var threeButton: UIButton!
@@ -24,51 +21,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var eightButton: UIButton!
     @IBOutlet weak var nineButton: UIButton!
     @IBOutlet weak var zeroButton: UIButton!
-    
-    
     @IBOutlet weak var dotButton: UIButton!
     @IBOutlet weak var percentButton: UIButton!
     @IBOutlet weak var equalButton: UIButton!
-    
     @IBOutlet weak var plusButton: UIButton!
     @IBOutlet weak var minusButton: UIButton!
     @IBOutlet weak var multiButton: UIButton!
     @IBOutlet weak var divideButton: UIButton!
-    
     @IBOutlet weak var clearButton: UIButton!
     @IBOutlet weak var signToggleButton: UIButton!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
 }
 
 extension ViewController {
-    
     @IBAction func didNumberButtonPressed(_ sender: UIButton) {
         if isOperationNow {
             inputString = nil
             isOperationNow.toggle()
         }
-        var appended: String? = sender.titleLabel?.text
-        
-        if let inputString = inputString , let appended = appended{
+        let appended: String? = sender.titleLabel?.text
+        if let inputString = inputString,
+            let appended = appended {
             self.inputString! += appended
         } else {
             self.inputString = appended
         }
-    
         textLabel.text = inputString
     }
-    
     @IBAction func didOperationButtonPressed(_ sender: UIButton) {
         if let inputString = inputString {
             Calculation.calculator.pushNumber(inputString)
         }
-        
-        var buttonText: String? = sender.titleLabel?.text
-        
+        let buttonText: String? = sender.titleLabel?.text
         if let buttonText = buttonText {
             isOperationNow.toggle()
             let calculationValue = Calculation.calculator.pushOperator(buttonText)
@@ -77,13 +60,9 @@ extension ViewController {
             }
         }
     }
-    
     @IBAction func didClearButtonPressed(_ sender: UIButton) {
         Calculation.calculator.allClear()
         textLabel.text = "0"
         inputString = nil
     }
-    
-    
 }
-
